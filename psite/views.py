d4fc03137997af3b5c9c2d493bca34a5ad1043d2
Shreenath Bhanderi <skbhanderi3@gmail.com>
2020-03-25 16:09:32 -0400

    homepage fixes

--- psite/views.py
+++ psite/views.py
@@ -16,7 +16,7 @@ def post(request, pk):
     return render(request, 'post.html', {'post':post})
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def about_me(request):
     return render(request, 'about_me.html')
